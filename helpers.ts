78489e85a57cd57c01ad2849b8ab9acc30add48f
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:38:13 +0600

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:38:20.638Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-22T17:38:26.621Z
+Commit: Resolve timezone issues in date handling
