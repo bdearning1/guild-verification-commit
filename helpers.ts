b3bba7f6bf7083f1836844c72bf08a27516620de
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 22:05:13 +0600

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T16:04:53.105Z
-Commit: Add dark mode support
+Timestamp: 2026-04-22T16:05:27.189Z
+Commit: Resolve null pointer exception in user service
