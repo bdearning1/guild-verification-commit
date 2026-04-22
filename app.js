8e8aaf9667cf9d7791ad09744fa9350a68f246b4
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 22:04:44 +0600

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T16:04:27.099Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-04-22T16:04:58.056Z
+Commit: Fix broken links in documentation
