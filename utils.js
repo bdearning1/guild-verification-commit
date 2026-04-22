7f111af12157c7d20d9bee403fb26a52d6e0db13
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:41:36 +0600

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:41:11.812Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-22T17:41:50.064Z
+Commit: Patch API endpoint error handling
