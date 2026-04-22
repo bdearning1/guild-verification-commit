03cc7649a15af7e74f3368fac6126d209e2df726
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:40:28 +0600

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:40:32.114Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-22T17:40:41.712Z
+Commit: Fix memory leak in event handlers
