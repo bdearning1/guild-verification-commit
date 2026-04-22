29d843dd63c3c34119a0a8bff81391b3ddc572ef
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:40:06 +0600

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:38:56.560Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-22T17:40:20.098Z
+Commit: Resolve null pointer exception in user service
