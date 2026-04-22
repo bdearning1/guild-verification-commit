b1813e749c64393b07f901e81e9c36186b3d1229
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:40:24 +0600

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:40:20.098Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-22T17:40:38.079Z
+Commit: Add multi-language support
