6f497fe08d83bc612e48f13f4df6f53b28453d86
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:38:22 +0600

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:38:14.648Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-22T17:38:36.156Z
+Commit: Resolve null pointer exception in user service
