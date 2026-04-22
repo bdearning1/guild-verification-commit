df6a50cdfd37825ac2eb5b729f1749b4a4d4fff9
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:38:42 +0600

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:38:26.621Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-22T17:38:56.560Z
+Commit: Implement user profile customization
