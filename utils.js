2578ddeae62e79ecfd6382e13931d7c9219c5128
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:39:30 +0600

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:38:54.225Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-04-22T17:39:44.298Z
+Commit: Fix CSS styling bug on mobile devices
