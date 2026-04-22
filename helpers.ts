0d88e71d5068c35c091181d2e631f176832154c6
bdearning1 <161415998+bdearning1@users.noreply.github.com>
2026-04-22 23:41:57 +0600

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T17:42:02.017Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-22T17:42:11.756Z
+Commit: Patch security vulnerability in authentication
